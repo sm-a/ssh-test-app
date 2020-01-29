594c913ef89dc91edd59ff2ac853f856d3238cfa
Sven-Meikel Auer <sma@auerdev.com>
2020-01-29 22:21:00 +0100

    fixed

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -1,20 +1,11 @@
-﻿using System;
+﻿using Renci.SshNet;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-using Renci.SshNet;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -30,25 +21,46 @@ namespace App1
             this.InitializeComponent();
         }
 
-        private void btnStart_Click(object sender, RoutedEventArgs e)
+        private SynchronizationContext sc = SynchronizationContext.Current;
+        private bool IsInProgress { get; set; } = false; // Zur Verhinderung von mehrfachen Klicks
+        private List<String> smaCommands;
+        private String smaHost;
+        private String smaUserName;
+        private String smaPW;
+
+        private async void btnStart_Click(object sender, RoutedEventArgs e)
         {
+            if (IsInProgress) return; // wenn "In Arbeit", dann abbrechen
+            this.IsInProgress = true; // "In Arbeit" setzen
+
             // Testen ob alle Felder ausgefüllt wurden
             if (smaHostTF.Text == "" || smaUserNameTF.Text == "" || smaPWTF.Text == "")
             {
                 Debug.WriteLine("NICHT ALLE FELDER SIND AUSGEFÜLLT - WIRD DAHER ABGEBROCHEN!");
-                return;
+                //return;
             }
 
-            String smaHost = smaHostTF.Text.Trim();
-            String smaUserName = smaUserNameTF.Text.Trim();
-            String smaPW = smaPWTF.Text.Trim();
-
-            List<String> smaCommands = new List<String>();
+            smaCommands = new List<String>();
 
             smaCommands.Add("sleep 15");
             smaCommands.Add("sleep 10");
             smaCommands.Add("sleep 5");
 
+            // progressbar initialisierung mit werten
+            pbStatus.Maximum = smaCommands.Count;
+            pbStatus.Value = 0;
+
+            smaHost = smaHostTF.Text.Trim();
+            smaUserName = smaUserNameTF.Text.Trim();
+            smaPW = smaPWTF.Text.Trim();
+
+            await Task.Run(new Action(DoWork));
+        }
+
+
+
+        private void DoWork()
+        {
             ConnectionInfo conInfo = new ConnectionInfo(smaHost, 22, smaUserName, new AuthenticationMethod[]{
                     new PasswordAuthenticationMethod(smaUserName,smaPW)
             });
@@ -56,10 +68,6 @@ namespace App1
             // ssh client verbinden
             SshClient sshClient = new SshClient(conInfo);
             sshClient.Connect();
-
-            // progressbar initialisierung mit werten
-            pbStatus.Maximum = smaCommands.Count;
-            pbStatus.Value = 0;
 
             foreach (var smaCommandsToProcess in smaCommands)
             {
@@ -70,11 +78,13 @@ namespace App1
 
                 Debug.WriteLine("Aktuelles Kommando OUTPUT:\n" + smaCommandsToProcess + "\n" + s1);
 
-                pbStatus.Value += 1;
+                sc.Post(new SendOrPostCallback((state) => pbStatus.Value += 1), null);
             }
 
             sshClient.Disconnect();
             sshClient.Dispose();
+
+            this.IsInProgress = false;
         }
     }
 }
